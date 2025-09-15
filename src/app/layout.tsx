36d941134242c4bb8d313de40101a1562712cc52
Sannyii Wang <34569814+sannyii@users.noreply.github.com>
2025-09-15 13:10:57 +0800

    Fix redirect loop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import { redirect } from 'next/navigation';
-
-export default function RootLayout() {
-  // 重定向到默认语言
-  redirect('/zh');
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
 }
